f2976f0a10adbe460d517b3e830eaf754c0c4714
Dennis Jekubczyk <dennis.jekubczyk@gmail.com>
2021-07-11 11:25:18 +0200

    bootstrap service

--- build/validate-preconditions.sh
+++ build/validate-preconditions.sh
@@ -2,8 +2,9 @@
 
 set -eu -o pipefail
 
-COMMANDS='curl lsof mktemp node npm screen'
+COMMANDS='curl go lsof mktemp node npm screen'
 NODE_VERSION='v14.'
+GO_VERSION='go1.16.'
 
 die() {
   local reason="$1"
@@ -18,10 +19,14 @@ require_commands() {
   done
 }
 
-require_node() {
+require_node_version() {
   node --version | grep "$NODE_VERSION" &> /dev/null || die "please install node with version $NODE_VERSION"
 }
 
+require_go_version() {
+  go version | grep "$GO_VERSION" &> /dev/null || die "please install node with version $GO_VERSION"
+}
 
 require_commands
-require_node
+require_node_version
+require_go_version
---
+++ service/go.mod
@@ -0,0 +1,3 @@
+module github.com/jaedle/time-track/service
+
+go 1.16
