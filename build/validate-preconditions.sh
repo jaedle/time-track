8808cb31e74f417c3dd164c1052e611ee92545bb
Dennis Jekubczyk <dennis.jekubczyk@gmail.com>
2021-07-11 10:06:28 +0200

    add more pre-build assertions

--- build/validate-preconditions.sh
+++ build/validate-preconditions.sh
@@ -2,6 +2,7 @@
 
 set -eu -o pipefail
 
+COMMANDS='npm node'
 NODE_VERSION='v14.'
 
 die() {
@@ -11,8 +12,16 @@ die() {
   exit 1
 }
 
+require_commands() {
+  for cmd in $COMMANDS; do
+    which "$cmd" &> /dev/null || die "command '$cmd' not found, please install"
+  done
+}
+
 require_node() {
   node --version | grep "$NODE_VERSION" &> /dev/null || die "please install node with version $NODE_VERSION"
 }
 
+
+require_commands
 require_node
