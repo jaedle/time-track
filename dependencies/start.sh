a8a8829aeec6a1bff0a4753bedb6146dd4fdc697
Dennis Jekubczyk <dennis.jekubczyk@gmail.com>
2021-07-17 13:01:34 +0200

    switch to mysql

--- dependencies/start.sh
+++ dependencies/start.sh
@@ -5,5 +5,5 @@ DEPENDENCIES_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 set -eu -o pipefail
 
 docker-compose up -d
-"$DEPENDENCIES_DIR/wait-mariadb-healthy.sh"
+"$DEPENDENCIES_DIR/wait-mysql-healthy.sh"
 
